a9118dad595d46e867076ca8dd22d9a85f955643
Parabeac-Nest <admin@parabeac.com>
2022-10-25 20:21:33 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/test_first_page_colors.g.dart
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TestFirstPageColors {
+
+static const Color basicBasic900 = Color(0xff1a1a1a);
+
+static const Color mainMain = Color(0xff0074bc);
+
+static const Color basicBasic600 = Color(0xff666666);
+
+static const Color basicBasic0 = Color(0xffffffff);
+
+static const Color colorsMainBasic0 = Color(0xffffffff);
+}
